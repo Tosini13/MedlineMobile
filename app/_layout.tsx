99fa0e40f076c1c682e532d2fb2851058fd137b3
Tosini13 <jbartos13@gmail.com>
2024-05-08 10:53:26 +0200

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,31 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Link, Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
+import { Feather } from "@expo/vector-icons";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Box, NativeBaseProvider, Text } from "native-base";
+import React from "react";
+import { Pressable } from "react-native";
+
+const queryClient = new QueryClient();
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -22,7 +33,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -48,11 +59,63 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <NativeBaseProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="(modal)/menu"
+              options={{ presentation: "modal", title: "Menu" }}
+            />
+            <Stack.Screen
+              name="(modal)/search"
+              options={{
+                presentation: "modal",
+                title: "Search",
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="modal"
+              options={{ presentation: "modal", headerShown: false }}
+            />
+            <Stack.Screen
+              name="lines/index"
+              options={{
+                title: "Lines",
+                headerStyle: {
+                  backgroundColor: "#608ae3",
+                },
+                headerLeft: () => (
+                  <Link href="/(modal)/search" asChild>
+                    <Pressable className="center mx-1 flex h-9 w-9 items-center justify-center rounded-lg bg-[#4d74c8]">
+                      <Feather name="search" size={20} color="white" />
+                    </Pressable>
+                  </Link>
+                ),
+                headerRight: () => (
+                  <Link href="/(modal)/menu" asChild>
+                    <Pressable className="center mx-1 flex h-9 w-9 items-center justify-center rounded-lg bg-[#4d74c8]">
+                      <Feather name="menu" size={20} color="white" />
+                    </Pressable>
+                  </Link>
+                ),
+                headerTitle: () => (
+                  <Box>
+                    <Text className="text-center font-semibold text-white">
+                      Welcome back!
+                    </Text>
+                    <Text className="text-center text-white">
+                      How are you feeling today?
+                    </Text>
+                  </Box>
+                ),
+              }}
+            />
+          </Stack>
+        </NativeBaseProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
