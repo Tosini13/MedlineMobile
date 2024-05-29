98ffe3a5c44455f133fc826515b25ecb72461ebf
Tosini13 <jbartos13@gmail.com>
2024-05-29 18:24:50 +0200

    implement popover instead of menu

--- components/Header/EventHeaderSettingsButton.tsx
+++ components/Header/EventHeaderSettingsButton.tsx
@@ -1,23 +1,19 @@
-import { FC } from "react";
-import { ActivityIndicator } from "react-native";
+import React, { FC, useState } from "react";
 
-import { Text } from "@/components/Themed";
 import { invokeAsyncWithDelay } from "@/helpers/helpers";
 import { LineType } from "@/types";
-import { Feather, MaterialIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Link, useRouter } from "expo-router";
+import { useRouter } from "expo-router";
 
-import React from "react";
 import { Pressable } from "react-native";
 
-import {
-  Menu,
-  MenuOption,
-  MenuOptions,
-  MenuTrigger,
-} from "react-native-popup-menu";
+import { Box, Popover } from "native-base";
+import { ActivityIndicator } from "react-native";
 import { defaultHeaderButtonProps } from "./HeaderButton";
+
+import { Text } from "@/components/Themed";
+import { MaterialIcons } from "@expo/vector-icons";
 
 type EventHeaderSettingsButtonPropsType = {
   lineId: string;
@@ -26,6 +22,7 @@ type EventHeaderSettingsButtonPropsType = {
 const EventHeaderSettingsButton: FC<EventHeaderSettingsButtonPropsType> = ({
   lineId,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
 
   const queryClient = useQueryClient();
@@ -40,35 +37,72 @@ const EventHeaderSettingsButton: FC<EventHeaderSettingsButtonPropsType> = ({
 
       queryClient.setQueryData(["line", lineId], updateLines);
       queryClient.setQueryData(["lines"], updateLines);
+      setIsOpen(false);
       router.navigate("/lines");
     },
   });
 
+  const items = [
+    <Pressable
+      accessibilityLabel="Edit line"
+      className="flex h-full w-full flex-row items-center gap-x-4"
+      onPress={() => {
+        setIsOpen(false);
+        router.navigate(`/lines/${lineId}/edit`);
+      }}
+    >
+      <MaterialIcons name="edit" size={26} color="black" />
+      <Text className="text-xl">Edit</Text>
+    </Pressable>,
+    <Pressable
+      accessibilityLabel="Delete event"
+      className="flex h-full w-full flex-row  items-center gap-x-4"
+      onPress={() => mutate(lineId)}
+    >
+      {isPending ? (
+        <ActivityIndicator color="black" />
+      ) : (
+        <MaterialIcons name="delete" size={26} color="black" />
+      )}
+      <Text className="text-xl">Delete</Text>
+    </Pressable>,
+  ];
+
   return (
-    <Menu>
-      <MenuTrigger style={defaultHeaderButtonProps.style}>
-        <Feather
-          name="settings"
-          size={24}
-          color={defaultHeaderButtonProps.icon.color}
-        />
-      </MenuTrigger>
-      <MenuOptions>
-        <MenuOption>
-          <Link href={`/lines/${lineId}/edit`} asChild>
-            <Pressable
-              accessibilityLabel="Edit line"
-              className="flex flex-row items-center gap-x-4"
-            >
-              <MaterialIcons name="edit" size={26} color="black" />
-              <Text className="text-xl">Edit</Text>
-            </Pressable>
-          </Link>
-        </MenuOption>
-        <MenuOption>
+    <Popover
+      trigger={(triggerProps) => (
+        <Pressable
+          {...triggerProps}
+          style={defaultHeaderButtonProps.style}
+          onPress={() => setIsOpen(true)}
+        >
+          <Feather
+            name="settings"
+            size={24}
+            color={defaultHeaderButtonProps.icon.color}
+          />
+        </Pressable>
+      )}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(!isOpen)}
+    >
+      <Popover.Content w="40" className="bg-white">
+        <Popover.Body className="m-0 flex flex-col items-stretch p-0">
+          <Pressable
+            accessibilityLabel="Edit line"
+            className="flex w-full flex-row items-center gap-x-4 px-4 py-3"
+            onPress={() => {
+              setIsOpen(false);
+              router.navigate(`/lines/${lineId}/edit`);
+            }}
+          >
+            <MaterialIcons name="edit" size={26} color="black" />
+            <Text className="text-xl">Edit</Text>
+          </Pressable>
+          <Box className="h-[1px] bg-gray-300" />
           <Pressable
             accessibilityLabel="Delete event"
-            className="flex flex-row items-center gap-x-4"
+            className="flex w-full flex-row  items-center gap-x-4 px-4 py-3"
             onPress={() => mutate(lineId)}
           >
             {isPending ? (
@@ -78,9 +112,9 @@ const EventHeaderSettingsButton: FC<EventHeaderSettingsButtonPropsType> = ({
             )}
             <Text className="text-xl">Delete</Text>
           </Pressable>
-        </MenuOption>
-      </MenuOptions>
-    </Menu>
+        </Popover.Body>
+      </Popover.Content>
+    </Popover>
   );
 };
 
