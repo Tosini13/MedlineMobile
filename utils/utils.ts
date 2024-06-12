5e68c53afd0406df9b381cf90c2d511bb941bb0d
Tosini13 <jbartos13@gmail.com>
2024-06-12 18:22:34 +0200

    initialize firebase and add envs

---
+++ utils/utils.ts
@@ -0,0 +1,10 @@
+export const envs = {
+  apiKey: process.env.EXPO_API_KEY,
+  authDomain: process.env.EXPO_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_DATABASE_URL,
+  projectId: process.env.EXPO_PROJECT_ID,
+  storageBucket: process.env.EXPO_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_APP_ID,
+  measurementId: process.env.EXPO_MEASUREMENT_ID,
+};
