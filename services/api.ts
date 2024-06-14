f359cfd0d306f39c5787a1872bdc2d23bd5cf3ce
Tosini13 <jbartos13@gmail.com>
2024-06-14 21:48:55 +0200

    add firestore functions to api object

--- services/api.ts
+++ services/api.ts
@@ -1,19 +1,35 @@
 import { db } from "@/firebaseConfig";
 import { EventType, LineType } from "@/types";
+import { addEvent } from "./addEvent";
+import { addLine } from "./addLine";
+import { deleteEvent } from "./deleteEvent";
+import { deleteLine } from "./deleteLine";
 import { getEvent } from "./getEvent";
 import { getEvents } from "./getEvents";
 import { getLine } from "./getLine";
 import { getLines } from "./getLines";
+import { updateEvent } from "./updateEvent";
+import { updateLine } from "./updateLine";
 
 export const API = {
   lines: {
     get: () => getLines(db) as Promise<LineType[]>,
     getById: (lineId: string) =>
       getLine(db, lineId) as Promise<LineType | null>,
+    add: (line: Omit<LineType, "id">) => addLine(db, line) as Promise<LineType>,
+    update: (lineId: string, line: Omit<LineType, "id">) =>
+      updateLine(db, lineId, line) as Promise<LineType>,
+    delete: (lineId: string) => deleteLine(db, lineId) as Promise<string>,
   },
   events: {
     get: (lineId: string) => getEvents(db, lineId) as Promise<EventType[]>,
     getById: (lineId: string, eventId: string) =>
       getEvent(db, lineId, eventId) as Promise<EventType | null>,
+    add: (lineId: string, event: Omit<EventType, "id">) =>
+      addEvent(db, lineId, event) as Promise<EventType>,
+    update: (lineId: string, eventId: string, event: Omit<EventType, "id">) =>
+      updateEvent(db, lineId, eventId, event) as Promise<EventType>,
+    delete: (lineId: string, eventId: string) =>
+      deleteEvent(db, lineId, eventId) as Promise<string>,
   },
 };
