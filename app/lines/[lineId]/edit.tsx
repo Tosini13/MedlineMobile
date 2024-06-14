b31187da9e3dfe5bb0c512214a5593abf0200447
Tosini13 <jbartos13@gmail.com>
2024-06-14 21:51:45 +0200

    implement update line api

--- app/lines/[lineId]/edit.tsx
+++ app/lines/[lineId]/edit.tsx
@@ -1,8 +1,8 @@
 import LineForm, { LineFormType } from "@/components/LineForm/LineForm";
 import { setLineFormTitleData } from "@/helpers/headerHelpers";
-import { invokeAsyncWithDelay } from "@/helpers/helpers";
 import { API } from "@/services/api";
 import { LineType } from "@/types";
+import { envs, returnPromiseError } from "@/utils/utils";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useLocalSearchParams, useNavigation, useRouter } from "expo-router";
 import { Box } from "native-base";
@@ -35,23 +35,23 @@ const EditLine: FC<EditLinePropsType> = ({}) => {
   const queryClient = useQueryClient();
   const { mutate, isPending: isMutationPending } = useMutation({
     mutationFn: (values: LineFormType) =>
-      invokeAsyncWithDelay<LineType | undefined>(() =>
-        lineId
-          ? {
-              ...values,
-              id: lineId,
-              ownerId: "mock",
-            }
-          : undefined,
-      ),
+      lineId
+        ? API.lines.update(lineId, {
+            ...values,
+            ownerId: envs.testOwnerId,
+          })
+        : returnPromiseError("Line id is missing"),
     onSuccess: (line) => {
       if (line) {
-        const updateLineEvents = (old: LineType[]) =>
-          old.map((e) => (e.id === line.id ? line : e));
-
-        queryClient.setQueryData(["line", lineId], updateLineEvents);
-        queryClient.setQueryData(["lines"], updateLineEvents);
-        router.navigate(`/lines/${lineId}/events`);
+        try {
+          queryClient.setQueryData(["line", lineId], () => line);
+          queryClient.setQueryData(["lines"], (old: LineType[]) =>
+            old.map((e) => (e.id === line.id ? line : e)),
+          );
+          router.navigate(`/lines/${lineId}/events`);
+        } catch (e) {
+          console.log("e", e);
+        }
       }
     },
   });
