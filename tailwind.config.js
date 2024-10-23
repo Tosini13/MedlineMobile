0e476f0d43524285d7c3783d9cac8e52f1b702e2
Tosini13 <jbartos13@gmail.com>
2024-10-23 14:56:08 +1100

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors: {
         primary: "#fff",
-        "primary-accent": "#F5F7FF", //DFE1E7
+        "primary-accent": "#F5F7FF",
+        "primary-accent-2": "#DFE1E7",
         secondary: "#041A41",
         "secondary-accent": "#7E89A0",
         tint: "#036DFF",
