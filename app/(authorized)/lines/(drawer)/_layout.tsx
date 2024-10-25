53b37228d8f1e4a88e637f96be935bf1dc457d82
Tosini13 <jbartos13@gmail.com>
2024-10-25 18:22:33 +1100

    remove unused code

--- app/(authorized)/lines/(drawer)/_layout.tsx
+++ app/(authorized)/lines/(drawer)/_layout.tsx
@@ -3,22 +3,13 @@ import { useThemeColor } from "@/components/Themed";
 import { useAuthContext } from "@/context/auth.context";
 import { API } from "@/services/api";
 import { routes } from "@/utils/utils";
-import { Feather, MaterialIcons } from "@expo/vector-icons";
-import {
-  DrawerContentScrollView,
-  DrawerItem,
-  DrawerItemList,
-} from "@react-navigation/drawer";
+import { Feather } from "@expo/vector-icons";
 import { useMutation } from "@tanstack/react-query";
 import { Redirect, Stack, useRouter } from "expo-router";
-import { Drawer } from "expo-router/drawer";
-import { Box } from "native-base";
 import { FC } from "react";
-import { ActivityIndicator, Pressable } from "react-native";
+import { Pressable } from "react-native";
 
-type LayoutPropsType = {};
-
-const Layout: FC<LayoutPropsType> = ({}) => {
+const Layout: FC = () => {
   const bg = useThemeColor({}, "background");
   const text = useThemeColor({}, "text");
   const { isLoggedIn } = useAuthContext();
@@ -70,66 +61,6 @@ const Layout: FC<LayoutPropsType> = ({}) => {
             );
           },
         })}
-      />
-    </>
-  );
-  return (
-    <>
-      <Drawer.Screen
-        options={{
-          title: "Lines",
-          headerShown: false,
-        }}
-      />
-      <Drawer
-        initialRouteName={routes.lines}
-        screenOptions={({ navigation }) => ({
-          headerTitleAlign: "center",
-          headerStyle: {
-            backgroundColor: bg,
-          },
-          headerTintColor: text,
-          headerLeft: () => (
-            <Box className="ml-3">
-              <Logo />
-            </Box>
-          ),
-          headerRight: () => {
-            const isOpen = !!navigation
-              .getState()
-              .history.find(
-                (item: { type: string; status: string }) =>
-                  item.type === "drawer" && item.status === "open",
-              );
-            return (
-              <Pressable
-                onPress={() => navigation.toggleDrawer()}
-                className="mr-2 flex h-11 w-11 items-center justify-center"
-              >
-                <Feather name="menu" size={24} />
-              </Pressable>
-            );
-          },
-        })}
-        backBehavior="firstRoute"
-        drawerContent={(props) => {
-          return (
-            <DrawerContentScrollView {...props}>
-              <DrawerItemList {...props} />
-              <DrawerItem
-                label="Logout"
-                onPress={() => mutate()}
-                icon={({ color, size }) =>
-                  isPending ? (
-                    <ActivityIndicator color={color} size={size} />
-                  ) : (
-                    <MaterialIcons name="logout" color={color} size={size} />
-                  )
-                }
-              />
-            </DrawerContentScrollView>
-          );
-        }}
       />
     </>
   );
